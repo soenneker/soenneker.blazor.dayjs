39ae2f2c9b1e6d75c59b816df15f48f1497a6836
Jake Soenneker <jake@soenneker.com>
2026-01-25 09:09:19 -0600

    Test

---
+++ test/Soenneker.Blazor.Dayjs.Tests/DayJsInteropTests.cs
@@ -0,0 +1,23 @@
+﻿using Soenneker.Blazor.Dayjs.Abstract;
+using Soenneker.Blazor.Dayjs.Tests;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Blazor.TomSelect.Tests;
+
+[Collection("Collection")]
+public class TomSelectInteropTests : FixturedUnitTest
+{
+    private readonly IDayJsInterop _util;
+
+    public TomSelectInteropTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _util = Resolve<IDayJsInterop>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
